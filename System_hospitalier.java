ab3645e13b9e6296299fb98264be31725f2238ee
Fremony <126704054+Fremony@users.noreply.github.com>
2023-03-02 12:55:21 +0100

    "Des nouveaux fichiers ont été ajouté"

---
+++ System_hospitalier.java
@@ -0,0 +1,9 @@
+ private String  analyserRequete(Requête){
+	//la methode prend un objet de classe Requête en paramètre et analyse cette requête
+	return "requête analysée";
+}
+
+private String comparerDonnes(Données){
+	//la methode ne prend pas de paramètres,  compare les données
+	return "Cluster trouvé";
+}
